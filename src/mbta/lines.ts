da6f52c8b43c4a329d965bb17a4a2d26eb9ba3fc
JD Rudie <rudiejd@miamioh.edu>
2024-10-21 00:31:17 -0400

    still can't get it right

--- src/mbta/lines.ts
+++ src/mbta/lines.ts
@@ -60,47 +60,47 @@ function getStationIndex(stations, stationName: string): number {
 
 export const createGreenLineDiagram = (options: CreateDiagramOptions = {}) => {
     const { pxPerStation = DEFAULT_PX_PER_STATION } = options;
-    const startB: Turtle = { x: 20, y: 0, theta: 90 };
-    const startC: Turtle = { x: 0, y: 0, theta: 90 };
-    const startD: Turtle = { x: -20, y: 0, theta: 90 };
-    const startE: Turtle = { x: -40, y: 0, theta: 90 };
+    const startB: Turtle = { x: 40, y: 0, theta: 90 };
+    const startC: Turtle = { x: 0, y: -40, theta: 90 };
+    const startD: Turtle = { x: -40, y: -80, theta: 90 };
+    const startE: Turtle = { x: -80, y: -20, theta: 90 };
     const stationsE = getStationsForLine('Green', 'E').reverse();
 
-    const copleyIndex = getStationIndex(stationsE, 'place-coecl');
-    const govtCenterIndex = getStationIndex(stationsE, 'place-gover');
     // you could calculate this more easily if you just figured out all the places where
     // every branch is served... no need to over engineer though
-    const stationsTrunk = stationsE.slice(govtCenterIndex, copleyIndex + 1);
-    const stationsEBeforeTrunk = stationsE.slice(0, copleyIndex + 1);
-
-    const haymarketIdx = getStationIndex(stationsE, 'place-haecl');
-
-    const stationsEAfterTrunk = stationsE.slice(haymarketIdx + 1);
-
-    const trunkCommand = line(pxPerStation * stationsTrunk.length, ['trunk']);
+    const stationsTrunkForE = stationsE.slice(
+        getStationIndex(stationsE, 'place-coecl'),
+        getStationIndex(stationsE, 'place-lech') + 1,
+    );
+    const stationsEBeforeTrunk = stationsE.slice(0, getStationIndex(stationsE, 'place-coecl'));
+    console.log(stationsEBeforeTrunk);
+    const stationsEAfterTrunk = stationsE.slice(getStationIndex(stationsE, 'place-lech') + 1);
+    console.log(stationsEAfterTrunk);
 
     const stationsD = getStationsForLine('Green', 'D').reverse();
-    const stationsDBeforeTrunk = stationsD.slice(0, getStationIndex(stationsD, 'place-coecl') + 1);
-    console.log(stationsDBeforeTrunk);
-    const stationsDAfterTrunk = stationsD.slice(getStationIndex(stationsD, 'place-haecl') + 1);
-    console.log(stationsDAfterTrunk);
+
+    const kenmoreIdxD = getStationIndex(stationsD, 'place-kencl');
+    const stationsTrunkForBCD = stationsD.slice(kenmoreIdxD, getStationIndex(stationsD, 'place-lech') + 1);
+    const trunkCommandBCD = line(stationsTrunkForBCD.length);
+    const stationsDBeforeTrunk = stationsD.slice(0, getStationIndex(stationsD, 'place-coecl'));
+    const stationsDAfterTrunk = stationsD.slice(getStationIndex(stationsD, 'place-lech') + 1);
 
     const stationsC = getStationsForLine('Green', 'C').reverse();
-    const stationsCBeforeTrunk = stationsC.slice(0, getStationIndex(stationsC, 'place-coecl') + 1);
+    const stationsCBeforeTrunk = stationsC.slice(0, getStationIndex(stationsC, 'place-kencl'));
 
     const stationsB = getStationsForLine('Green', 'B').reverse();
-    const stationsBBeforeTrunk = stationsB.slice(0, getStationIndex(stationsB, 'place-coecl') + 1);
+    const stationsBBeforeTrunk = stationsB.slice(0, getStationIndex(stationsB, 'place-kencl'));
 
     const pathB = execute({
         start: startB,
         ranges: ['branch-b-stations'],
-        commands: [line(pxPerStation * stationsBBeforeTrunk.length), wiggle(15, -20), trunkCommand],
+        commands: [line(pxPerStation * stationsBBeforeTrunk.length), wiggle(15, -40), trunkCommandBCD],
     });
 
     const pathC = execute({
         start: startC,
         ranges: ['branch-c-stations'],
-        commands: [line(pxPerStation * stationsCBeforeTrunk.length), trunkCommand],
+        commands: [line(pxPerStation * stationsCBeforeTrunk.length), line(40), trunkCommandBCD],
     });
 
     const pathD = execute({
@@ -108,8 +108,9 @@ export const createGreenLineDiagram = (options: CreateDiagramOptions = {}) => {
         ranges: ['branch-d-stations'],
         commands: [
             line(pxPerStation * stationsDBeforeTrunk.length),
-            wiggle(15, 20),
-            trunkCommand,
+            wiggle(15, 40),
+            trunkCommandBCD,
+            wiggle(15, 40),
             line(pxPerStation * stationsDAfterTrunk.length),
         ],
     });
@@ -119,14 +120,14 @@ export const createGreenLineDiagram = (options: CreateDiagramOptions = {}) => {
         ranges: ['branch-e-stations'],
         commands: [
             line(pxPerStation * stationsEBeforeTrunk.length),
-            wiggle(15, 40),
-            trunkCommand,
+            line(20),
+            wiggle(30, 80),
+            line(pxPerStation * stationsTrunkForE.length),
             line(pxPerStation * stationsEAfterTrunk.length),
         ],
     });
 
-    return new Diagram([pathE, pathD, pathC, pathB], {
-        trunk: stationsTrunk,
+    return new Diagram([pathB, pathC, pathD, pathE], {
         'branch-b-stations': stationsB,
         'branch-c-stations': stationsC,
         'branch-d-stations': stationsD,
