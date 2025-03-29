e605e96f759e27e48ea9539c547b1f97656c1c41
Devin Matte <devinmatte@gmail.com>
2025-03-29 13:44:00 -0400

    Add Mattapan line map (#19)
    * Add Mattapan line map
    
    Also cleans up Storybook
    
    * Allow using DiagrammableLineName
    
    * Fixing station stats

--- src/components/LineMap/LineMap.stories.tsx
+++ src/components/LineMap/LineMap.stories.tsx
@@ -1,75 +1,107 @@
 import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
 
 import type { SegmentRenderOptions } from './LineMap';
 import { LineMap } from './LineMap';
 import { createDefaultDiagramForLine } from '../..';
 
-export default {
+const meta: Meta = {
     title: 'LineMap',
     component: LineMap,
+    render: (args) => {
+        const { line, direction } = args;
+        const diagram = createDefaultDiagramForLine(line);
+
+        const lineColor = line === 'Mattapan' ? 'red': line.toLowerCase();
+
+        return (
+            <>
+                <LineMap
+                    direction={direction}
+                    diagram={diagram}
+                    getStationLabel={(options) => options.stationId}
+                    strokeOptions={{ stroke: lineColor }}
+                    getSegments={() => []}
+                />
+            </>
+        );
+    },
+    argTypes: {
+        line: {
+            options: ['Red', 'Green', 'Orange', 'Blue', 'Mattapan'],
+            defaultValue: 'Red',
+            control: { type: 'radio' },
+        },
+        direction: { options: ['horizontal', 'vertical'], defaultValue: 'horizontal', control: { type: 'radio' } },
+        diagram: { control: { disable: true } },
+        getSegments: { control: { disable: true } },
+        segments: { control: { disable: true }, defaultValue: [] },
+    },
 };
 
-const redLine = createDefaultDiagramForLine('Red');
-const greenLine = createDefaultDiagramForLine('Green');
-const redLineSegments: SegmentRenderOptions[] = [
-    {
-        location: {
-            toStationId: 'place-cntsq',
-            fromStationId: 'place-pktrm',
+export const RedLine = () => {
+    const diagram = createDefaultDiagramForLine('Red');
+    const redLineSegments: SegmentRenderOptions[] = [
+        {
+            location: { toStationId: 'place-cntsq', fromStationId: 'place-pktrm' },
+            strokes: [
+                { offset: 1, stroke: 'red', opacity: 0.1 },
+                { offset: -1, stroke: 'red', opacity: 0.3 },
+                { offset: -2, stroke: 'red', opacity: 0.6 },
+                { offset: -3, stroke: 'red', opacity: 0.8 },
+            ],
+            labels: [
+                {
+                    mapSide: '0',
+                    boundingSize: 40,
+                    content: () => (
+                        <div style={{ fontSize: 4 }}>Greetings amigos thank you for inviting me into your SVG</div>
+                    ),
+                },
+                {
+                    mapSide: '1',
+                    boundingSize: 40,
+                    content: () => (
+                        <div style={{ fontSize: 4 }}>And on this side too! I also like being on this side!</div>
+                    ),
+                },
+            ],
         },
-        strokes: [
-            { offset: 1, stroke: 'red', opacity: 0.1 },
-            { offset: -1, stroke: 'red', opacity: 0.3 },
-            { offset: -2, stroke: 'red', opacity: 0.6 },
-            { offset: -3, stroke: 'red', opacity: 0.8 },
-        ],
-        labels: [
-            {
-                mapSide: '0',
-                boundingSize: 40,
-                content: () => (
-                    <div style={{ fontSize: 4 }}>Greetings amigos thank you for inviting me into your SVG</div>
-                ),
-            },
-            {
-                mapSide: '1',
-                boundingSize: 40,
-                content: () => <div style={{ fontSize: 4 }}>And on this side too! I also like being on this side!</div>,
-            },
-        ],
-    },
-    {
-        location: {
-            toStationId: 'place-pktrm',
-            fromStationId: 'place-shmnl',
+        {
+            location: { toStationId: 'place-pktrm', fromStationId: 'place-shmnl' },
+            strokes: [
+                { offset: 1, stroke: 'red', opacity: 0.3 },
+                { offset: 2, stroke: 'red', opacity: 0.6 },
+                { offset: 3, stroke: 'red', opacity: 0.8 },
+            ],
         },
-        strokes: [
-            { offset: 1, stroke: 'red', opacity: 0.3 },
-            { offset: 2, stroke: 'red', opacity: 0.6 },
-            { offset: 3, stroke: 'red', opacity: 0.8 },
-        ],
-    },
-];
+    ];
 
-const greenLineSegments: SegmentRenderOptions[] = [];
-
-export const Main = () => {
     return (
-        <>
-            <LineMap
-                direction='horizontal'
-                diagram={greenLine}
-                strokeOptions={{ stroke: 'green' }}
-                getSegments={() => greenLineSegments}
-            />
-
-            <LineMap
-                direction='horizontal'
-                diagram={redLine}
-                getStationLabel={(options) => options.stationId}
-                strokeOptions={{ stroke: 'red' }}
-                getSegments={() => redLineSegments}
-            />
-        </>
+        <LineMap
+            direction='horizontal'
+            diagram={diagram}
+            getStationLabel={(options) => options.stationId}
+            strokeOptions={{ stroke: 'red' }}
+            getSegments={() => redLineSegments}
+        />
     );
 };
+
+export const GreenLine = () => {
+    const greenLineSegments: SegmentRenderOptions[] = [];
+    const greenLine = createDefaultDiagramForLine('Green');
+
+    return (
+        <LineMap
+            direction='horizontal'
+            diagram={greenLine}
+            strokeOptions={{ stroke: 'green' }}
+            getSegments={() => greenLineSegments}
+        />
+    );
+};
+
+export const Main: StoryObj = { args: { line: 'Red', direction: 'horizontal' } };
+
+export default meta;
